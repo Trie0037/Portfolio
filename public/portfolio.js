7959acd7c8fbe56077078ec83ce7737e1684bf77
Trie0037 <trie0037@gmail.com>
2019-02-06 15:35:23 -0600

    unbreak

--- public/portfolio.js
+++ public/portfolio.js
@@ -9,21 +9,16 @@ $(document).ready(function() {
     messagingSenderId: "536429628411"
   };
   firebase.initializeApp(config);
+
   var database = firebase.database();
 
   $("#submit-btn").click(function() {
     //Creating variables for contact information
-    var yourName = $("#your-name")
-      .val()
-      .trim();
-    var yourEmail = $("#your-email")
-      .val()
-      .trim();
-    var Employer = $("#Employer")
-      .val()
-      .trim();
+    var yourName = $("#your-name").val().trim();
+    var yourEmail = $("#your-email").val().trim();
+    var Employer = $("#Employer").val().trim();
     console.log(yourName);
-    alert("works");
+    // alert("works");
 
     database.ref().push({
       Name: yourName,
@@ -61,28 +56,28 @@ $(document).ready(function() {
 });
 
 // Modal content
-var modal = document.getElementById("resumeModal");
+// var modal = document.getElementById("resumeModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("resumeBtn");
+// var btn = document.getElementById("resumeBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-};
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
 // When the user clicks the close button,
 // remove the modal and disable the submit button
-$("#close").on("click", function() {
-  modal.style.display = "none";
-});
+// $("#close").on("click", function() {
+//   modal.style.display = "none";
+// });
